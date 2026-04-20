db9fa736f3ad51efac8fc41950a37a557fd9b3cc
Aaditya Joshi <aaditya.joshi@gatech.edu>
2026-04-19 23:59:23 -0400

    Modularize the code, support other kernel configs.

--- statReader.cpp
+++ statReader.cpp
@@ -1,4 +1,11 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <unistd.h>
+#include <cstdint>
 using namespace std;
 
 struct CoreData
@@ -8,30 +15,38 @@ struct CoreData
     uint64_t prevTotal;
     uint64_t currIdle;
     uint64_t currTotal;
+    double cpuUsage;
 };
 
-void readProcStatData(ifstream &reader, CoreData &coreData)
+bool readProcStatData(stringstream &ss, CoreData &coreData)
 {
-    uint64_t user, nice, system, idle, ioWait, irq, softirq, steal;
-    string data, ignoreFirst;
-    getline(reader, data);
-    stringstream ss(data);
-    ss >> ignoreFirst >> user >> nice >> system >> idle >> ioWait >> irq >> softirq >> steal;
+    uint64_t user = 0, nice = 0, system = 0, idle = 0, ioWait = 0, irq = 0, softirq = 0, steal = 0;
+    string ignoreFirst;
+    if (!(ss >> ignoreFirst >> user >> nice >> system >> idle))
+    {
+        return 0;
+    }
+    ss >> ioWait >> irq >> softirq >> steal;
     coreData.currIdle = idle + ioWait;
     coreData.currTotal = user + nice + system + idle + ioWait + irq + softirq + steal;
+    return 1;
+}
+
+void calculateCpuUsage(CoreData &coreData)
+{
+    double deltaIdle = coreData.currIdle - coreData.prevIdle;
+    double deltaTotal = coreData.currTotal - coreData.prevTotal;
+    if (deltaTotal > 0)
+        coreData.cpuUsage = ((double)(deltaTotal - deltaIdle) / deltaTotal) * 100;
+    else
+        coreData.cpuUsage = 0.0;
+    coreData.prevTotal = coreData.currTotal;
+    coreData.prevIdle = coreData.currIdle;
 }
 
 void outputCpuUsage(CoreData &coreData)
 {
-    double deltaIdle = coreData.currIdle - coreData.prevIdle;
-    double deltaTotal = coreData.currTotal - coreData.prevTotal;
-    cout << coreData.name << ": ";
-    if (deltaTotal > 0)
-        cout << ((double)(deltaTotal - deltaIdle) / deltaTotal) * 100 << " ";
-    else
-        cout << 0.0 << " ";
-    coreData.prevTotal = coreData.currTotal;
-    coreData.prevIdle = coreData.currIdle;
+    cout << coreData.name << ": " << coreData.cpuUsage << " ";
 }
 
 int main()
@@ -70,8 +85,17 @@ int main()
         fileReader.seekg(0);
         for (auto &coreData : coreDataArr)
         {
-            readProcStatData(fileReader, coreData);
-            outputCpuUsage(coreData);
+            getline(fileReader, data);
+            stringstream ss(data);
+            if (readProcStatData(ss, coreData))
+            {
+                calculateCpuUsage(coreData);
+                outputCpuUsage(coreData);
+            }
+            else
+            {
+                cout << "ERR: " << coreData.name << " ";
+            }
         }
         cout << "\n";
         sleep(1);
